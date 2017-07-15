4afad346a887a1870a1c4af8f7c0351f690e41d1
Andy Holst <andy.holst85@gmail.com>
2017-07-15 06:36:12 +0200

    Updated code for CoreComponent

--- pipefilters/src/main/java/com/design/pipefilters/filters/CoreComponent.java
+++ pipefilters/src/main/java/com/design/pipefilters/filters/CoreComponent.java
@@ -57,11 +57,7 @@ public class CoreComponent extends Thread implements Component {
     public void run() {
         while (!pipe.isClosed()) {
             if (pipe.isPipeMessagePrepared()) {
-                byte[] bytes = pipe.getQueue().remove();
-
-                if (port.validateMessage(bytes)) {
-                    message.append(new String(bytes, StandardCharsets.UTF_8));
-                }
+                message.append(new String(pipe.getQueue().remove(), StandardCharsets.UTF_8));
 
                 if (pipe.getQueue().isEmpty()) {
                     pipe.setClosed(true);
